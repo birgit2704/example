d0201fdb4ba5b00aec37b03921343102c0aea109
Birgit <birgit.bartenschlager@web.de>
2023-10-22 20:16:40 +0200

    initial commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddToCard from "./AddToCard";
+
+const ProductCard = () => {
+  return (
+    <div className="p-5 my-5 bg-slate-500 text-white text-xl hover:bg-slate-800">
+      <AddToCard />
+    </div>
+  );
+};
+
+export default ProductCard;
